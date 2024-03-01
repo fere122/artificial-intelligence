3d10970bd939cea78520136886ea5347324b95a0
fere122 <06blake.krueger@gmail.com>
2024-03-01 08:37:35 -0500

    Added user list

--- AI Playground.py
+++ AI Playground.py
@@ -3,3 +3,8 @@
 #Program: AI Playground
 
 print("This will be a place for me to play with programming using AI technology\n")
+
+Name = ['corey', 'chris', 'travis', 'dave']
+
+for index, name in enumerate(Name):
+    print(index, name)
